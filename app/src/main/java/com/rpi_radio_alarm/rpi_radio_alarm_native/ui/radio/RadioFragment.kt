ab07c78723c703a72b6ba627dc1e44b7e872b76b
bb4L <39266013+bb4L@users.noreply.github.com>
2020-11-20 09:03:43 +0100

    Add basic layout

---
+++ app/src/main/java/com/rpi_radio_alarm/rpi_radio_alarm_native/ui/radio/RadioFragment.kt
@@ -0,0 +1,32 @@
+package com.rpi_radio_alarm.rpi_radio_alarm_native.ui.radio
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rpi_radio_alarm.rpi_radio_alarm_native.R
+
+class RadioFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RadioFragment()
+    }
+
+    private lateinit var viewModel: RadioViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.radio_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(RadioViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/rpi_radio_alarm/rpi_radio_alarm_native/ui/radio/RadioViewModel.kt
@@ -0,0 +1,7 @@
+package com.rpi_radio_alarm.rpi_radio_alarm_native.ui.radio
+
+import androidx.lifecycle.ViewModel
+
+class RadioViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
